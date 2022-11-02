afbc3f5fbb3525b02d98ddb34b4f1d84f82e264b
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-11-02 22:04:15 +0100

    add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+// Tailwind not natively support by turbopack yet
+import "../styles/dist.css";
+
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
